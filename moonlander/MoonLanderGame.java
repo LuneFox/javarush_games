ef82d6f913784dc3e7391fe018b076615adab27f
LuneFox <lunefox@msn.com>
2020-05-06 19:06:06 +0300

    Moonlander step 14/23

--- moonlander/MoonLanderGame.java
+++ moonlander/MoonLanderGame.java
@@ -6,21 +6,112 @@ public class MoonLanderGame extends Game {
 
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    private Rocket rocket;
+    private GameObject landscape;
+    private boolean isUpPressed;
+    private boolean isLeftPressed;
+    private boolean isRightPressed;
 
     @Override
     public void initialize() {
+        showGrid(false);
         setScreenSize(WIDTH, HEIGHT);
         createGame();
     }
 
-    private void createGame() {
+    @Override
+    public void onTurn(int step) {
+        rocket.move(isUpPressed, isLeftPressed, isRightPressed);
+        check();
         drawScene();
+    }
+
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if (x < 0 || y < 0 || x > WIDTH - 1 || y > HEIGHT - 1) {
+            return;
+        }
+        super.setCellColor(x, y, color);
+    }
+
+    private void createGame() {
+        setTurnTimer(50);
+        createGameObjects();
+        drawScene();
+        isUpPressed = false;
+        isLeftPressed = false;
+        isRightPressed = false;
+    }
+
+    private void createGameObjects() {
+        rocket = new Rocket(WIDTH / 2, 0);
+        landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
     }
 
     private void drawScene() {
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
-                setCellColor(x, y, Color.ORANGE);
+                setCellColor(x, y, Color.DARKBLUE);
+            }
+        }
+        rocket.draw(this);
+        landscape.draw(this);
+    }
+
+    // GAME MECHANICS
+
+    private void check() {
+
+    }
+
+    private void win() {
+
+    }
+
+    private void gameOver() {
+
+    }
+
+    // CONTROLS
+
+    @Override
+    public void onKeyPress(Key key) {
+        switch (key) {
+            case UP: {
+                isUpPressed = true;
+                break;
+            }
+            case LEFT: {
+                isRightPressed = false;
+                isLeftPressed = true;
+                break;
+            }
+            case RIGHT: {
+                isLeftPressed = false;
+                isRightPressed = true;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onKeyReleased(Key key) {
+        switch (key) {
+            case UP: {
+                isUpPressed = false;
+                break;
+            }
+            case LEFT: {
+                isLeftPressed = false;
+                break;
+            }
+            case RIGHT: {
+                isRightPressed = false;
+            }
+            default: {
+                break;
             }
         }
     }
--- moonlander/Rocket.java
+++ moonlander/Rocket.java
@@ -1,4 +1,77 @@
 package com.javarush.games.moonlander;
 
-public class Rocket {
+import com.javarush.engine.cell.Color;
+
+public class Rocket extends GameObject {
+    private double speedY = 0;
+    private double speedX = 0;
+    private double boost = 0.05;
+    private double slowdown = boost / 10;
+
+    public Rocket(double x, double y) {
+        super(x, y, ShapeMatrix.ROCKET);
+    }
+
+    public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed) {
+        if (isUpPressed) {
+            speedY -= boost;
+        } else {
+            speedY += boost;
+        }
+        y += speedY;
+
+        if (isLeftPressed) {
+            speedX -= boost;
+            x += speedX;
+        } else if (isRightPressed) {
+            speedX += boost;
+            x += speedX;
+        } else if (speedX > slowdown) {
+            speedX -= slowdown;
+        } else if (speedX < -slowdown) {
+            speedX += slowdown;
+        } else {
+            speedX = 0;
+        }
+        x += speedX;
+        checkBorders();
+    }
+
+    private void checkBorders() {
+        if (x < 0) {
+            x = 0;
+            speedX = 0;
+        } else if (x + width > MoonLanderGame.WIDTH) {
+            x = MoonLanderGame.WIDTH - width;
+            speedX = 0;
+        }
+        if (y <= 0) {
+            y = 0;
+            speedY = 0;
+        }
+    }
+
+    public boolean isStopped() {
+        return speedY < 10 * boost;
+    }
+
+    public boolean isCollision(GameObject object) {
+        int transparent = Color.NONE.ordinal();
+
+        for (int matrixX = 0; matrixX < width; matrixX++) {
+            for (int matrixY = 0; matrixY < height; matrixY++) {
+                int objectX = matrixX + (int) x - (int) object.x;
+                int objectY = matrixY + (int) y - (int) object.y;
+
+                if (objectX < 0 || objectX >= object.width || objectY < 0 || objectY >= object.height) {
+                    continue;
+                }
+
+                if (matrix[matrixY][matrixX] != transparent && object.matrix[objectY][objectX] != transparent) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
