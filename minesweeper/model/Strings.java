5e33776580ed2fb6b6b41a14bce60f23e5ebe3ed
Lune Fox <lunefox@msn.com>
2022-03-08 20:44:02 +0300

    Verion release: 1.21

--- minesweeper/model/Strings.java
+++ minesweeper/model/Strings.java
@@ -7,7 +7,7 @@ import com.javarush.games.minesweeper.model.options.Options;
  */
 
 public class Strings {
-    public static final String VERSION = "1.20";
+    public static final String VERSION = "1.21";
 
     public static final String[] RECORDS = new String[]{
             "Лучшие игроки",
