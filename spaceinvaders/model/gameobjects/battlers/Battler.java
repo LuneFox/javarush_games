2e49312a005f2af6e9b74311f5a69b78e3826899
Lune Fox <lunefox@msn.com>
2022-05-18 15:19:09 +0300

    Removed redundant variable.

--- spaceinvaders/model/gameobjects/battlers/Battler.java
+++ spaceinvaders/model/gameobjects/battlers/Battler.java
@@ -1,15 +1,12 @@
 package com.javarush.games.spaceinvaders.model.gameobjects.battlers;
 
-import com.javarush.games.spaceinvaders.SpaceInvadersGame;
-import com.javarush.games.spaceinvaders.model.gameobjects.GameObject;
 import com.javarush.games.spaceinvaders.model.Shooter;
+import com.javarush.games.spaceinvaders.model.gameobjects.GameObject;
 import com.javarush.games.spaceinvaders.model.gameobjects.bullets.Bullet;
 
 import java.util.Optional;
 
 public abstract class Battler extends GameObject implements Shooter {
-    protected static SpaceInvadersGame game;
-
     public Battler(double x, double y) {
         super(x, y);
     }
@@ -18,9 +15,5 @@ public abstract class Battler extends GameObject implements Shooter {
 
     public void kill() {
         isAlive = false;
-    }
-
-    public static void setGame(SpaceInvadersGame game) {
-        Battler.game = game;
     }
 }
