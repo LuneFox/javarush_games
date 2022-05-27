1f44c7abaf7468016c007994c760bdb736ed134d
Lune Fox <lunefox@msn.com>
2022-05-27 21:02:40 +0300

    Switch replaces with if (more visual)

--- game2048/model/BallUtils.java
+++ game2048/model/BallUtils.java
@@ -5,33 +5,24 @@ import com.javarush.engine.cell.Color;
 public class BallUtils {
     public static final char[] BALL_SYMBOLS = " ❶❷❸❹❺❻❼❽➈➉⑪⑫⑬⑭⑮⬤".toCharArray();
 
-    public static Color getColorForBallNumber(int ballNumber) {
-        switch (ballNumber) {
-            case 1:
-            case 9:
-                return Color.YELLOW;
-            case 2:
-            case 10:
-                return Color.MEDIUMBLUE;
-            case 3:
-            case 11:
-                return Color.RED;
-            case 4:
-            case 12:
-                return Color.PURPLE;
-            case 5:
-            case 13:
-                return Color.ORANGE;
-            case 6:
-            case 14:
-                return Color.LAWNGREEN;
-            case 7:
-            case 15:
-                return Color.SADDLEBROWN;
-            case 8:
-                return Color.BLACK;
-            default:
-                return Color.WHITE;
+    public static Color getColorForBallNumber(int number) {
+        if (number == 1 || number == 9) {
+            return Color.YELLOW;
+        } else if (number == 2 || number == 10) {
+            return Color.MEDIUMBLUE;
+        } else if (number == 3 || number == 11) {
+            return Color.RED;
+        } else if (number == 4 || number == 12) {
+            return Color.PURPLE;
+        } else if (number == 5 || number == 13) {
+            return Color.ORANGE;
+        } else if (number == 6 || number == 14) {
+            return Color.LAWNGREEN;
+        } else if (number == 7 || number == 15) {
+            return Color.SADDLEBROWN;
+        } else if (number == 8) {
+            return Color.BLACK;
         }
+        return Color.WHITE;
     }
 }
